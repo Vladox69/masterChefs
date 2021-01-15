fa443d19d88770b5b8b0f0079a4160d7a7954994
jess026p <jesifv1999@gmail.com>
2021-01-15 13:12:01 -0800

    creando funciones en el scripts

--- filtrar recteas por categorias/js/scripts.js
+++ filtrar recteas por categorias/js/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
     alert('holi.. Estas seguro de actualizar la pagina?');
+/*crear funciones */
+$(' .categor_item').click(function () {
+
+    
+
+
+});
+
+//mostrando todos los productos
+
+   
+
 });
