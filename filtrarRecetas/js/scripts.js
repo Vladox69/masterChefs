b056756c9a8d58b1733604de3aa00b5d10340f21
jess026p <jesifv1999@gmail.com>
2021-01-21 00:52:35 -0800

    creacion de alerta en cada actualizacion, clase active al enlace

---
+++ filtrarRecetas/js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    alert('holi.. Estas seguro de actualizar la pagina?');
+    //agregar la clase active al primer enlace
+    $(' .category_list .categor_item[category="all"]').addClass('ct_item-active');
+});
