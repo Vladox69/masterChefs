e2cc36cd25cf21c70b75463ca67289c5a68ad95a
jess026p <jesifv1999@gmail.com>
2021-01-21 00:59:21 -0800

    agregando tranciciones y filtro de botones

--- filtrarRecetas/js/scripts.js
+++ filtrarRecetas/js/scripts.js
@@ -2,4 +2,35 @@ $(document).ready(function () {
     alert('holi.. Estas seguro de actualizar la pagina?');
     //agregar la clase active al primer enlace
     $(' .category_list .categor_item[category="all"]').addClass('ct_item-active');
+
+    $(' .categor_item').click(function () {
+
+         //filtrando categorias
+
+         var catProdcut = $(this).attr('category');
+         console.log(catProdcut);
+ 
+         //agregando clases active al boton seleccionado
+         $(' .categor_item').removeClass('ct_item-active');
+         $(this).addClass('ct_item-active');
+ 
+         // filtrar sin efectos- ocultado products
+         //ocultando productos -hide()
+ 
+         $('.recet-item').css('transform', 'scale(0)');
+         function hideProduct() {
+             $('.recet-item').hide();
+         } setTimeout(hideProduct, 400);
+ 
+ 
+ 
+         // mostrar productos - show()
+ 
+         function showProduct() {
+             $(' .recet-item[category="' + catProdcut + '"]').show();
+             $(' .recet-item[category="' + catProdcut + '"]').css('transform', 'scale(1)');
+ 
+         } setTimeout(showProduct, 400);
+ 
+    });
 });
