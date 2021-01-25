d84981de1e780c50245ce1c6d37490d71dd08314
jess026p <jesifv1999@gmail.com>
2021-01-25 12:43:58 -0800

    modificacion de menu y alerta en las paginas

--- filtrarRecetas/js/scripts.js
+++ filtrarRecetas/js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    alert('holi.. Estas seguro de actualizar la pagina?');
+    // Aqui se encuentra la alerta de actualizacion de cada pagina
+    alert('Estas seguro de actualizar la pagina?');
     //agregar la clase avtive al primer enlace
     $(' .category_list .categor_item[category="all"]').addClass('ct_item-active');
     // FILTRANDO PRODUCTOS =======================================================================
